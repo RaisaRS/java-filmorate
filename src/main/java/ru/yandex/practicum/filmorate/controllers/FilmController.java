f8668941457026726df65d937bc08f84814e00cd
Raisa <raenka07@mail.ru>
2023-05-03 20:45:31 +0300

    Т№10 7

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -21,49 +21,49 @@ public class FilmController {
 
     @PostMapping
     public Film addFilm(@Valid @RequestBody Film film) {
-        log.info("Получен POST запрос");
+        log.info("Получен POST {} запрос /films ", film);
         return filmService.createFilm(film);
     }
 
     @PutMapping
     public Film putFilm(@Valid @RequestBody Film film) {
-        log.info("Получен PUT запрос");
+        log.info("Получен PUT {} запрос /films ", film);
         return filmService.updateFilm(film);
     }
 
     @DeleteMapping
     public Film deleteFilm(@Valid @RequestBody Film film) {
-        log.info("Получен DELETE запрос");
+        log.info("Получен DELETE {} запрос /films ", film);
         return filmService.deleteFilm(film);
     }
 
     @PutMapping("/{id}/like/{userId}")
     public void addLike(@PathVariable("id") long filmId, @PathVariable long userId) {
-        log.info("Получен PUT запрос");
+        log.info("Получен PUT {} запрос: /{id}/like/{userId} ", userId);
         filmService.addLike(filmId, userId);
     }
 
     @GetMapping
     public Collection<Film> filmsList() {
-        log.info("Получен GET запрос");
+        log.info("Получен GET запрос /films ");
         return filmService.filmList();
     }
 
     @GetMapping("/popular")
     public Collection<Film> listPopularFilms(@RequestParam(defaultValue = "10") int count) {
-        log.info("Получен GET запрос");
+        log.info("Получен GET {} запрос: /popular ", count);
         return filmService.listPopularFilms(count);
     }
 
     @GetMapping("/{id}")
     public Film getOneFilm(@PathVariable Long id) {
-        log.info("Получен GET запрос");
+        log.info("Получен GET {} запрос: /{id} ", id);
         return filmService.getOneFilm(id);
     }
 
     @DeleteMapping("/{id}/like/{userId}")
     public void removeLike(@PathVariable("id") long filmId, @PathVariable long userId) {
-        log.info("Получен DELETE запрос");
+        log.info("Получен DELETE {} запрос: /{id}/like/{userId} ", userId);
         filmService.deleteLike(filmId, userId);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -24,52 +24,52 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping()
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public User addUser(@RequestBody @Valid User user) { //добавление пользователя
-        log.info("POST request received: {}", user);
+        log.info("POST request received: {} /users ", user);
         return userService.addUser(user);
     }
 
     @PutMapping
     public User update(@RequestBody @Valid User user) {  //обновление подьзователя
-        log.info("Получен PUT запрос.");
+        log.info("Получен PUT запрос {} /users.", user);
         return userService.updateUser(user);
     }
 
-    @GetMapping()
+    @GetMapping
     public Collection<User> usersList() { //список всех пользователей
-        log.info("Получен GET запрос.");
+        log.info("Получен GET запрос /users");
         return userService.usersList();
     }
 
     @PutMapping("/{id}/friends/{friendId}") //добавление в друзья
     public void addFriend(@PathVariable("id") Long userId, @PathVariable Long friendId) {
-        log.info("Получен PUT запрос.");
+        log.info("Получен PUT запрос {} : /{id}/friends/{friendId}. ", friendId);
         userService.addFriend(userId, friendId);
     }
 
     @DeleteMapping("/{id}/friends/{friendId}") //удаление из друзей
     public void deleteFriend(@PathVariable("id") Long userId, @PathVariable Long friendId) {
-        log.info("Получен DELETE запрос.");
+        log.info("Получен DELETE запрос {}: /{id}/friends/{friendId}. ", friendId);
         userService.deleteFriend(userId, friendId);
     }
 
     @GetMapping("/{id}")  //получение пользователя по идентификатору
     public User getOneUser(@PathVariable Long id) {
-        log.info("Получен GET запрос.");
+        log.info("Получен GET запрос {} : /{id}. ", id);
         return userService.getOneUser(id);
     }
 
     @GetMapping("/{id}/friends") //вывести список всех друзей одного пользователя
     public Collection<User> getAllUserFriends(@PathVariable Long id) {
-        log.info("Получен GET запрос.");
+        log.info("Получен GET запрос {} : /{id}/friends. ", id);
         return userService.getAllUserFriends(id);
     }
 
     @GetMapping("/{id}/friends/common/{otherId}")
     public Collection<User> listOfMutualFriends(@PathVariable long id, @PathVariable long otherId) {
-        log.info("Получен GET запрос.");
+        log.info("Получен GET запрос {} : /{id}/friends/common/{otherId}. ", otherId);
         return userService.listOfMutualFriends(id, otherId);
     }
 }
